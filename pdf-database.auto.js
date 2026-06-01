dc790b0fe63dd009c33daf4e8b98f832fa4cbc87
Lex <101748165+LexGit-Hub@users.noreply.github.com>
2026-06-01 15:11:08 +0300

    Add files via upload

--- pdf-database.auto.js
+++ pdf-database.auto.js
@@ -1,13 +1,25 @@
 var pdfDatabaseAuto = {
   "stiinte-naturii_2023_sesiunea1": [
     {
-      "id": "stiinte-naturii_2023_sesiunea1_SN-2",
-      "title": "SN 2",
-      "description": "Auto-detected PDF: SN-2.pdf",
-      "url": "/pdfs/stiinte-naturii/2023/sesiunea1/SN-2.pdf",
+      "id": "stiinte-naturii_2023_sesiunea1_matematica_stiintele-naturii_2023_sesiunea1_subiect",
+      "title": "matematica stiintele naturii 2023 sesiunea1 subiect",
+      "description": "Auto-detected PDF: matematica_stiintele-naturii_2023_sesiunea1_subiect.pdf",
+      "url": "pdfs/stiinte-naturii/2023/sesiunea1/matematica_stiintele-naturii_2023_sesiunea1_subiect.pdf",
       "date": ".",
       "size": "6.59 MB",
       "category": "sesiunea1",
+      "isPlaceholder": false
+    }
+  ],
+  "stiinte-naturii_2015_sesiunea2": [
+    {
+      "id": "stiinte-naturii_2015_sesiunea2_2015 MI S II",
+      "title": "2015 MI S II",
+      "description": "Auto-detected PDF: 2015 MI S II.pdf",
+      "url": "pdfs/stiinte-naturii/2015/sesiunea2/2015 MI S II.pdf",
+      "date": ".",
+      "size": "5.15 MB",
+      "category": "sesiunea2",
       "isPlaceholder": false
     }
   ]
